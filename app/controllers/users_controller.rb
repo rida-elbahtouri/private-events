1ee5a67c2779671d48d3aac88f37ca7ef2708329
8Bts <mcmizze@yahoo.com>
2020-07-30 13:58:36 +0400

    Add UsersController actions #new #create #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 def UsersController < ApplicationController
-    def show
+    def new
+        @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "User successfully created"
+            redirect_to @user
+            sign_in(@user.id)
+        else
+            flash[:alert] = "User couldn't be created"
+            render :new    
+        end    
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end 
+    
+    def sign_in(id)
+        if User.find(id)
+            session[:user_id] = id
+            return true
+        end
+        return false   
+    end    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :guests
   has_many :events ,through: :guests
   has_many :hosted_events, class_name: "Event", foreign_key: "creator_id"
+
+  validates :name, presence: true
 end
