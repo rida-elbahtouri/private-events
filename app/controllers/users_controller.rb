d3414d187baf77a2fcad4e5a1e96d278c63f58cb
8Bts <mcmizze@yahoo.com>
2020-07-30 14:57:15 +0400

    Add SessionsController and #new action with view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-def UsersController < ApplicationController
+class UsersController < ApplicationController
     def new
         @user = User.new
     end
@@ -6,18 +6,22 @@ def UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "User successfully created"
+            flash[:success] = "User with name #{@user.name} successfully created"
             redirect_to @user
-            sign_in(@user.id)
+            session[:current_user] = @user.id
         else
             flash[:alert] = "User couldn't be created"
             render :new    
-        end    
+        end
     end
 
     def show
         @user = User.find(params[:id])
     end
+
+    def login
+
+    end    
 
     private
 
@@ -25,11 +29,4 @@ def UsersController < ApplicationController
         params.require(:user).permit(:name)
     end 
     
-    def sign_in(id)
-        if User.find(id)
-            session[:user_id] = id
-            return true
-        end
-        return false   
-    end    
 end
